c81f50c2dae50024855f2805f15243cdec834cb4
Gabriel Falcade <80369496+GabrielFalcade97@users.noreply.github.com>
2021-11-08 20:57:01 -0300

    teste com faturas < 2000 (pass)

--- src/filtroFatura.js
+++ src/filtroFatura.js
@@ -1,6 +1,11 @@
 class FiltroFaturas {
     removerFaturasMenor2000(faturas) {
-        
+        var faturaMenor2000 = [];
+
+        faturas.forEach(element => {
+            if(element.valor >= 2000)
+                faturaMenor2000.push(element);
+        });
 
         return faturaMenor2000;
     }
